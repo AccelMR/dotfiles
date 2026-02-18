6bc6efe78228f64a22f83f3ee2e2479fe0bde9bc
accelmrLap <accelmr@proton.me>
2026-02-17 20:44:45 -0600

    Dotfiles init commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,135 @@
+## Set values
+# Hide welcome message & ensure we are reporting fish as shell
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x SHELL /usr/bin/fish
+
+## Export variable need for qt-theme
+if type qtile >>/dev/null 2>&1
+    set -x QT_QPA_PLATFORMTHEME qt5ct
+end
+
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+    source ~/.fish_profile
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.cargo/bin
+    if not contains -- ~/.cargo/bin $PATH
+        set -p PATH ~/.cargo/bin
+    end
+end
+
+## Starship prompt
+if status --is-interactive
+    source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
+else
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
+end
+
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# Copy DIR1 DIR2
+function copy
+    set count (count $argv | tr -d \n)
+    if test "$count" = 2; and test -d "$argv[1]"
+        set from (echo $argv[1] | string trim --right --chars=/)
+        set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
+end
+
+# Cleanup local orphaned packages
+function cleanup
+    while pacman -Qdtq
+        sudo pacman -R (pacman -Qdtq)
+    end
+end
+
+function fastfetch_min
+    if test "$argv[1]" = "foot"
+        /bin/fastfetch -c ~/.config/fastfetch/minimal_foot.jsonc
+    else if test "$argv[1]" = "kitty"
+        /bin/fastfetch -c ~/.config/fastfetch/minimal_kitty.jsonc
+    else
+        /bin/fastfetch -c ~/.config/fastfetch/minimal_gen.jsonc
+    end
+end
+
+
+function __set_fastfetch_config
+    set -l config
+    switch $TERM
+        case foot
+            set config ~/.config/fastfetch/foot.jsonc
+        case xterm-kitty
+            set config ~/.config/fastfetch/kitty.jsonc
+        case '*'
+            set config ''
+    end
+
+    if test -n "$config"
+        alias fastfetch "fastfetch --load-config $config"
+    end
+
+    if test -z "$ZELLIJ"
+        if test -n "$config"
+            fastfetch_min (basename $config .jsonc)
+        else
+            fastfetch_min
+        end
+    end
+end
+
+## Editor setup
+set -gx VISUAL code 
+set EDITOR /usr/bin/vscodium
+
+## Useful aliases
+
+# Replace ls with eza
+# alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
+alias ls 'eza --color=always --group-directories-first --icons' # preferred listing
+alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons' # long format
+alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+
+# Replace some more things with better alternatives
+alias cat 'bat --style header --style snip --style changes --style header'
+if not test -x /usr/bin/yay; and test -x /usr/bin/paru
+    alias yay paru
+end
+
+
+# Common use
+alias .. 'cd ..'
+
+# Get the error messages from journalctl
+alias jctl 'journalctl -p 3 -xb'
+
---
+++ .config/hypr/scripts/Volume.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+iDIR="$HOME/.config/dunst/icons"
+
+# Get icon based on volume
+get_icon() {
+    volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
+    if [[ "$volume" -eq 0 ]]; then
+        echo "$iDIR/volume-mute.png"
+    elif [[ "$volume" -lt 33 ]]; then
+        echo "$iDIR/volume-low.png"
+    elif [[ "$volume" -lt 66 ]]; then
+        echo "$iDIR/volume-medium.png"
+    else
+        echo "$iDIR/volume-high.png"
+    fi
+}
+
+# Get Volume
+get_volume() {
+    volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
+    muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o "MUTED")
+    
+    if [[ -n "$muted" ]]; then
+        echo "Muted"
+    else
+        echo "$volume%"
+    fi
+}
+
+# Toggle Mute
+toggle_mute() {
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    
+    muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o "MUTED")
+    if [[ -n "$muted" ]]; then
+        notify-send -e -a volume -h string:x-canonical-private-synchronous:volume_notif -h boolean:SWAYNC_BYPASS_DND:true -u low -i "$iDIR/volume-mute.png" "Volume Switched OFF"
+    else
+        notify-send -e -a volume -h string:x-canonical-private-synchronous:volume_notif -h boolean:SWAYNC_BYPASS_DND:true -u low -i "$(get_icon)" "Volume Switched ON"
+    fi
+}
+
+# Toggle Mic
+toggle_mic() {
+    wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+    
+    muted=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -o "MUTED")
+    if [[ -n "$muted" ]]; then
+        notify-send -e -h string:x-canonical-private-synchronous:volume_notif -h boolean:SWAYNC_BYPASS_DND:true -u low -i "$iDIR/microphone-mute.png" "Microphone Switched OFF"
+    else
+        notify-send -e -h string:x-canonical-private-synchronous:volume_notif -h boolean:SWAYNC_BYPASS_DND:true -u low -i "$iDIR/microphone.png" "Microphone Switched ON"
+    fi
+}
+
+# Execute accordingly
+if [[ "$1" == "--get" ]]; then
+    get_volume
+elif [[ "$1" == "--toggle" ]]; then
+    toggle_mute
+elif [[ "$1" == "--toggle-mic" ]]; then
+    toggle_mic
+else
+    get_volume
+fi
